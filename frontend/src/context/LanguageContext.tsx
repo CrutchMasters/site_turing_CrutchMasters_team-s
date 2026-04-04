50b7126221baf3b89d461b64d8aa74d763312c86
unknowstorm <1906.unknowstorm@gmail.com>
2026-04-04 17:12:41 +0300

    Update LanguageContext.tsx

--- frontend/src/context/LanguageContext.tsx
+++ frontend/src/context/LanguageContext.tsx
@@ -53,7 +53,7 @@ export const useT = () => {
 };
 
 export const LOCALES: { value: Locale; label: string;}[] = [
-  { value: "ua", label: "Українська",},
-  { value: "ru", label: "Русский", },
-  { value: "en", label: "English", },
+  { value: "ua", label: "Українська"},
+  { value: "ru", label: "Русский"},
+  { value: "en", label: "English"},
 ];
