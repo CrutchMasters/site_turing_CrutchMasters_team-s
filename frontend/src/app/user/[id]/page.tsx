2ffe5dd0ffbbf8c51dca47f9bae2e1f537cd9540
unknowstorm <1906.unknowstorm@gmail.com>
2026-04-07 00:05:20 +0300

    Update page.tsx

--- frontend/src/app/user/[id]/page.tsx
+++ frontend/src/app/user/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect, useMemo } from "react";
-import React, { useState, useEffect } from "react";
 import { useRouter, useParams } from "next/navigation";
 import {
   User, Mail, Shield, ChevronRight, UserCircle, ArrowLeft, Loader,
